75406e1ea1806e7a3586769bfe7b3460f54d5b5e
Furkan Doğan <furkan.dogan4238@gmail.com>
2024-08-21 15:11:18 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Pages } from "./components/pages/Pages";
+
+
+//npm install --save aos@next
+//aos
+
+function App() {
+  return (
+    <>
+      <Pages />
+    </>
+  );
+}
+
+export default App;
